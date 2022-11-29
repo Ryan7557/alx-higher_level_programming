d924a69a57102cf9a2272a3d4fe6ec1cacaa5fa4
Ryan7557 <muendesitawana@gmail.com>
2022-11-29 06:32:19 -0800

    ifel

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabeth in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(alphabeth), end="")
+for alpha in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(alpa), end="")
